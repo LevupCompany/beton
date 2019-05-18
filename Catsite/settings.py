b418c7e8dc9caa00c9fbbe72adcaf66baf81d6a2
LevupCompany <levup.company@gmail.com>
2019-05-18 11:44:37 +0300

    for test

--- Catsite/settings.py
+++ Catsite/settings.py
@@ -134,12 +134,9 @@ REDACTOR_UPLOAD = 'uploads/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-<<<<<<< Updated upstream
 
-=======
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
->>>>>>> Stashed changes
