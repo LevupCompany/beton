e42e91caf5e88b171e66df9164178de88495b22e
LevupCompany <levup.company@gmail.com>
2019-05-18 16:36:59 +0300

    Update settings.py

--- Catsite/settings.py
+++ Catsite/settings.py
@@ -131,13 +131,10 @@ REDACTOR_UPLOAD = 'uploads/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.django
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
