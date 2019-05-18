dea04d6e7b709318a2d0ff52a0ffe7284b3c1434
LevupCompany <levup.company@gmail.com>
2019-05-18 11:40:58 +0300

    for test

--- Catsite/settings.py
+++ Catsite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,4 +134,12 @@ REDACTOR_UPLOAD = 'uploads/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+<<<<<<< Updated upstream
 
+=======
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+>>>>>>> Stashed changes
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Catsite.wsgi --log-file -
